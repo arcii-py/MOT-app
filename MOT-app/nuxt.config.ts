5e1bf26a9239f41e9dde99dd26973ef9bd7d6b70
arcii-py <63791186+arcii-py@users.noreply.github.com>
2023-08-13 23:09:36 +0200

    possible working PWA

--- MOT-app/nuxt.config.ts
+++ MOT-app/nuxt.config.ts
@@ -1,5 +1,5 @@
 export default defineNuxtConfig({
-  
+
   modules: [
     '@nuxtjs/tailwindcss',
     '@vite-pwa/nuxt',
@@ -11,11 +11,33 @@ export default defineNuxtConfig({
       description: "test",
       icons: [
         {
-          src: 'static/ico.png',
+          src: 'icons/icon_64x64.png',
           sizes: '64x64',
+          type: 'image/png'
+        },
+        {
+          src: 'icons/icon_144x144.png',
+          sizes: '144x144',
+          type: 'image/png'
+        },
+        {
+          src: 'icons/icon_192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: 'icons/icon_512x512.png',
+          sizes: '512x512',
           type: 'image/png'
         }
       ]
-    }
-  }
+    },
+    workbox: {
+      navigateFallback: "/",
+    },
+    devOptions: {
+      enabled: true,
+      type: "module",
+    },
+  },
 });
